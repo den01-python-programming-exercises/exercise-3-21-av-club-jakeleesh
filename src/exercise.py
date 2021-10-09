861d1577cd3a8a72c141f9ec98255ae1f6e8a876
Jake Lee <jake.lee@fdmgroup.com>
2021-10-09 11:06:09 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    while True:
+        text = input()
+        if (text != ""):
+            text_split = text.split()
+            for word in text_split:
+                if "av" in word:
+                    print(word)
+        else:
+            break
 
 if __name__ == '__main__':
     main()
